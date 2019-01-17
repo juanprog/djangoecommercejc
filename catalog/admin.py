9d47f16851dea392b9f04c5e67241f01d1f968ef
Juan Souza <juan.sousa@dcomp.ufs.br>
2019-01-17 19:40:01 -0300

    Customizing django admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,6 +4,15 @@ from django.contrib import admin
 
 from .models import Product, Category
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created', 'modified']
+    search_field = ['name', 'slug']
+    list_filter = ['created', 'modified']
 
-admin.site.register(Category)
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'created', 'modified', 'price']
+    search_field = ['name', 'slug', 'category__name']
+    list_filter = ['created', 'modified']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- catalog/apps.py
+++ catalog/apps.py
@@ -1,5 +1,7 @@
-from django.apps import AppConfig
+# coding=utf-8
 
+from django.apps import AppConfig
 
 class CatalogConfig(AppConfig):
     name = 'catalog'
+    verbose_name = 'Catálogo'
