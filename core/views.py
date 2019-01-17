215b484a2e6dff7c916ebfee52aa22522d063a83
Juan Souza <juan.sousa@dcomp.ufs.br>
2019-01-17 09:52:59 -0300

    Cofiguring apps

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# coding=utf-8
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+
+def product(request):
+    return render(request, 'product.html')
--- djangoecommercejc/urls.py
+++ djangoecommercejc/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('contato/', views.contact, name='contact'),
+    path('produto/', views.product, name='product'),
+    path('produtos/', views.product_list, name='product_list'),
     path('admin/', admin.site.urls),
 ]
